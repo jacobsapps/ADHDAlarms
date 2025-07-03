1cf512a2e565405e9a8002395b0546b9ef614989
Jacob Bartlett <jacob@lapse.com>
2025-07-03 13:12:52 +0100

    Initial Commit

---
+++ ADHDAlarms/ADHDAlarmsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ADHDAlarmsApp.swift
+//  ADHDAlarms
+//
+//  Created by Jacob Bartlett on 03/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct ADHDAlarmsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
